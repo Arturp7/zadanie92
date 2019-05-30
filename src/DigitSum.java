2e0b7d112687b42faa54cd4ff21a407555823176
Artur Przyjemski <spinezpl@gmail.com>
2019-05-30 13:39:51 +0200

    second commit , variables x and digit removed from class DigitSum fields

--- src/DigitSum.java
+++ src/DigitSum.java
@@ -1,9 +1,8 @@
 public class DigitSum {
-    int x;
-    int digit;
+
 
     int sum(int x) {
-        digit = 0;
+        int digit = 0;
         while (x != 0) {
             digit = digit + x % 10;
             x = x / 10;
